83f20deeb56f7244765560105b4b19352686fb43
Behnam Rasoolian <behnam.rasoolian@coxautoinc.com>
2025-11-17 09:10:44 -0500

    old changes

--- dkutils/model.py
+++ dkutils/model.py
@@ -65,14 +65,14 @@ class ProphetModel:
         start_day = df.index.min()
         scale = 365.25 * 4
         t = (index - start_day).dt.days.values
-        self.S = gen_fourier_basis(t, n=2).astype("float32")
+        self.S = gen_fourier_basis(t, n=3).astype("float32")
         self.t = t / scale
         self.s = (
             trainInfo.changepoint[trainInfo.changepoint].index - start_day
         ).to_series().dt.days.values / scale
         self.A = (self.t[:, None] > self.s).astype("float32")
 
-        self.X = df[XCOLS].values.astype("float32")
+        self.X = jnp.array(df[XCOLS].values.astype("float32"))
         self.seasonality_switch = data.train.seasonality_switch.values
         self.n_seasonality_switches = self.seasonality_switch.max() + 1
         self.changepoints = trainInfo.changepoint.cumsum().values * 0
@@ -82,13 +82,10 @@ class ProphetModel:
 
         seasonality = yield from self.sample_seasonality(hierarchical=False)
         exogenous = yield from self.sample_exogenous()
-        economy = yield from self.sample_economy()
         trend = yield from self.sample_trend()
-        # intercept = yield from self.sample_intercept()
         noise_sigma = yield root(tfd.HalfNormal(scale=1.0, name="noise_sigma"))
-        # nu = yield tfd.Exponential(0.2, name="nu").root
 
-        y_hat = exogenous + seasonality + trend + economy
+        y_hat = seasonality + trend + exogenous
 
         y_hat = jnp.einsum("...n->n...", y_hat)[self.data.train.train.values]
         y_hat = jnp.einsum("n...->...n", y_hat)
@@ -99,33 +96,13 @@ class ProphetModel:
         )
 
     def get_exogenous(self, alpha, **kwargs):
-        # coeffs = jnp.array([1.0, 1, 1, -1, -1, 1, -1, -1, 1, 1, 1])
-        # alpha = coeffs * jnp.exp(alpha)
-        exogenous = jnp.einsum("ij,...j->...i", self.X[:, [0, 1, 10]], alpha)
+        exogenous = jnp.einsum("ij,...j->...i", self.X, alpha)
         return exogenous
 
     def sample_exogenous(self):
-        d = 3  # self.X.shape[-1]
-        # alpha_corr = yield tfd.CholeskyLKJ(d, 0.2, name="alpha_corr").root
-        # alpha_sigma = yield tfd.HalfNormal(1).Sample(d, name="alpha_sigma").root
-        # alpha_cov_tril = alpha_corr * alpha_sigma[..., None]
-
-        # alpha = yield tfd.MultivariateNormalTriL(0, alpha_cov_tril, name="alpha")
+        d = self.X.shape[-1]
         alpha = yield tfd.Normal(0, 0.5, name="alpha").Sample(d).root
         return self.get_exogenous(alpha)
-
-    # def sample_exogenous(self, f=1):
-    #     ex_b = yield tfd.Normal(0, 0.1, name="ex_b").Sample([self.X.shape[-1], f]).root
-    #     ex_c = yield tfd.Normal(0, 1, name="ex_c").Sample(self.X.shape[-1]).root
-    #     ex_eps = yield tfd.Normal(0, 1).Sample(self.X.shape[-1] + f, name="ex_eps").root
-    #     return self.get_exogenous(ex_b, ex_c, ex_eps, f)
-
-    # def get_exogenous(self, ex_b, ex_c, ex_eps, f=1, **kwargs):
-    #     alpha = ex_eps[..., :-f] * ex_c + jnp.einsum(
-    #         "...df,...f->...d", ex_b, ex_eps[..., -f:]
-    #     )
-    #     exogenous = jnp.einsum("ij,...j->...i", self.X, alpha)
-    #     return exogenous
 
     def get_seasonality(self, beta_0, beta_1, beta, **kwarg):
         beta = beta * beta_1[..., None] + beta_0[..., None]
@@ -167,9 +144,11 @@ class ProphetModel:
     def sample_trend(self):
         k = yield root(tfd.Normal(0, 1, name="k"))  # Slope at 0
         m = yield root(tfd.Normal(0, 1, name="m"))
-        tau = yield root(tfd.Exponential(20, name="tau"))
+        tau = yield root(tfd.Exponential(1, name="tau"))
         delta = yield root(
-            tfd.Sample(tfd.Laplace(0.0, 1), sample_shape=self.A.shape[1], name="delta")
+            tfd.Sample(
+                tfd.Laplace(0.0, tau), sample_shape=self.A.shape[1], name="delta"
+            )
         )
         return self.get_trend(k, m, tau, delta)
 
@@ -179,23 +158,34 @@ class ProphetModel:
         )
         return intercept.cumsum(axis=-1)[..., self.changepoints]
 
-    def sample_economy(self):
-        X = jnp.array(self.X[:, 2:-1])
-        initial_state = yield tfd.Normal(0, 1, name="hist_initial_state").root
-        A = yield tfd.Normal(0, 1).Sample(8, name="hist_A").root
-        b = yield tfd.Normal(0, 1).Sample(1, name="hist_b").root
-        O = yield tfd.Normal(0, 1, name="hist_O").Sample(2).root
+    def sample_economy(self, hidden_dim=1):
+        d = self.X.shape[-1]
+        initial_state = (
+            yield tfd.Normal(0, 1).Sample(hidden_dim, name="hist_initial_state").root
+        )
+        A = yield tfd.Normal(0, 1).Sample([d, hidden_dim], name="hist_A").root
+        A2 = (
+            yield tfd.Normal(0, 1).Sample([hidden_dim, hidden_dim], name="hist_A2").root
+        )
+        b = yield tfd.Normal(0, 1).Sample(hidden_dim, name="hist_b").root
+        O = yield tfd.Normal(0, 1, name="hist_O").Sample(hidden_dim + 1).root
+        return self.get_economy(initial_state, A, A2, b, O)
+
+    def get_economy(self, initial_state, A, A2, b, O, **kwargs):
 
         def step(carry, _):
             prev_state, i = carry
-            out = prev_state + jnp.einsum("...d,d->...", A, X[i])
+            prev_state = jnp.einsum("...dD,...d->...D", A2, prev_state)
+            out = jax.nn.sigmoid(
+                prev_state + jnp.einsum("...dk,d->...k", A, self.X[i]) + b
+            )
             return (out, i + 1), out
 
-        states = jax.nn.sigmoid(
+        states = jax.scipy.special.logit(
             jax.lax.scan(step, (initial_state, 0), length=len(self.X))[1]
         )
-        states = jnp.einsum("t...->...t", states)
-        return O[..., :1] * states + O[..., 1:]
+        states = jnp.einsum("t...k->...tk", states)
+        return jnp.einsum("...k,...tk->...t", O[..., :-1], states) + O[..., -1:]
 
     def __call__(self, pinned=True):
         data = self.data
